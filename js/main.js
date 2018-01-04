6f20b3a7ffc95eff6bd0d3d9d5fe4996510d051c
James MacLeod <james@macdoch.com>
2018-01-04 12:18:49 +1100

    aadding new build

--- js/main.js
+++ js/main.js
@@ -1,179 +1,217 @@
-let createTileBtn;
-let rowIndex;
-let board = [];
-let vertArrs = [];
-let diagArrs = [];
-
-let winnerArrX = ["X", "X", "X"];
-let winnerArrO = ["O", "O", "O"];
+let createBoard;
+let scoreBoard;
+let diagBoard;
+let playerMove;
+let togglePlayer;
+let clearBoard;
+let checkForWinner;
+let addEventHandlers;
 let player;
-let winnerArr;
-let winnerMessage;
-let oScore = 0;
-let xScore = 0;
-$("#player-o-score").text(oScore);
-$("#player-x-score").text(xScore);
+let opposition;
+let count;
+let message;
+let checkForDraw;
+let board = [];
 
 $("document").ready(function() {
-  const $startBtn = $("button#start-game-btn");
-  $startBtn.on("click", function() {
-    $(".controller").css("display", "none");
-    $(".play").css("display", "block");
-    ticTac.createBoard(3, 3, 0);
-    addEventHandlers();
-  });
+  let vertBoard = [],
+    lastRowCall,
+    lastColumnCall;
 
-  const $0select = $("#0-selector");
-  const $Xselect = $("#X-selector");
+  let Xscore = 0;
+  let Oscore = 0;
 
-  const toggleSelected = function(player, opposition) {
-    player.addClass("selected");
-    opposition.removeClass("selected");
+  getPlayer = function(count) {
+    if (count % 2 === 1) {
+      player = "X";
+      $("#X").addClass("selected");
+      $("#O").removeClass("selected");
+    } else {
+      player = "O";
+      $("#O").addClass("selected");
+      $("#X").removeClass("selected");
+    }
   };
 
-  $0select.on("click", function() {
-    toggleSelected($0select, $Xselect);
-    player = "oClicked";
-  });
+  message = function(string) {
+    $(".messageBoard,h3").text(string);
+  };
 
-  $Xselect.on("click", function() {
-    toggleSelected($Xselect, $0select);
-    player = "xClicked";
-  });
+  scoreBoard = function(player) {
+    if (player === "O") {
+      Oscore++;
+    }
+    if (player === "X") {
+      Xscore++;
+    }
+    $("#Oscore").html(Oscore);
+    $("#Xscore").html(Xscore);
+  };
 
-  const ticTac = {
-    //Tic Tac Object stores
-    count: 0,
-    switchPlayer: function() {
-      if ($Xselect.hasClass("selected")) {
-        toggleSelected($0select, $Xselect);
-        player = "xClicked";
-        winnerArr = winnerArrX;
-        winnerMessage = "X";
-      } else {
-        toggleSelected($Xselect, $0select);
-        player = "oClicked";
-        winnerArr = winnerArrO;
-        winnerMessage = "O";
+  getDiagBoard = function(board) {
+    diagBoard = [];
+    var W = board[0].length;
+    var H = board.length;
+    var starts = [];
+    // Horizontal
+    for (var i = 0; i < W; i++) {
+      starts.push([i, 0]);
+    }
+    // Vertical, start at 1, because we already have 0, 0
+    for (var i = 1; i < H; i++) {
+      starts.push([0, i]);
+    }
+
+    var validCoord = function(C) {
+      return C[0] >= 0 && C[0] < W && C[1] < H;
+    };
+
+    var getDiag = function(start, dx) {
+      var C = [start[0], start[1]]; // copy
+      var diag = [];
+      while (validCoord(C)) {
+        diag.push(board[C[1]][C[0]]);
+        C[0] += dx; // One to the left/right
+        C[1] += 1; // One down
       }
-    },
+      return diag;
+    };
 
-    createBoard: function($numberOfRows, $numberOfColumns, $winnerStreak) {
-      board = [];
-      for (let i = 0; i < $numberOfRows; i++) {
-        let rowArray = [];
-        $newRow = $("<div/>", { class: "gameRow", id: i });
-        $(".gameBoard").append($newRow);
-        for (let j = 0; j < $numberOfColumns; j++) {
-          let btnId = "" + i + "-" + j;
-          let newBtn = $("<button/>", {
-            class: "boardBtn",
-            id: btnId
-          });
-          let row = $("#" + i);
-          $("#" + i).append(newBtn);
-          rowArray.push(btnId);
-        }
-        board.push(rowArray);
-      }
-    },
+    starts.forEach(function(start) {
+      var diag;
+      diag = getDiag(start, 1);
+      diag.length > 1 && diagBoard.push(diag);
+      diag = getDiag(start, -1);
+      diag.length > 1 && diagBoard.push(diag);
+    });
+    return diagBoard;
+  };
 
-    boardRules: function(btnId, player) {
-      console.log(btnId);
+  addClickHandlers = function() {
+    $(".boardBtn").on("click", function() {
+      let btnId = event.target.id;
       btnId = btnId.split("-");
-      let x = btnId[0];
-      let y = btnId[1];
-      vertArrs = [];
-      diagArrs = [];
-      for (let k = 0; k < 3; k++) {
-        vertArrs.push([]);
-        for (let i = 0; i < 3; i++) {
-          vertArrs[k].push(board[i][k]);
-        }
-      }
-      counter = 0;
-      for (let k = 0; k < 2; k++) {
-        diagArrs.push([]);
-        counter++;
-        if (counter === 1) {
-          for (let i = 0; i < 3; i++) {
-            diagArrs[0].push(board[i][i]);
-          }
-        }
-        if (counter === 2) {
-          for (let u = 0; u < 3; u++) {
-            o = board.length - u;
-            diagArrs[k].push(board[u][o - 1]);
-          }
-        }
-      }
+      let rowIndex = btnId[0];
+      let colIndex = btnId[1];
+      playerMove(player, rowIndex, colIndex);
+    });
+  };
 
+  createBoard = function(numberOfRows, numberOfColumns) {
+    (board = []), (vertBoard = []);
+    lastRowCall = numberOfRows;
+    lastColumnCall = numberOfColumns;
+    $(".gameBoard").empty();
+    for (let i = 0; i < numberOfRows; i++) {
+      const row = [],
+        column = [];
+      $newRow = $("<div/>", { class: "gameRow", id: i });
+      $(".gameBoard").append($newRow);
+      for (let j = 0; j < numberOfColumns; j++) {
+        row.push(`${i}-${j}`);
+        column.push(`${j}-${i}`);
+        let newBtn = $("<button/>", {
+          class: "boardBtn",
+          id: `${i}-${j}`
+        });
+        let $thisRow = $("#" + i);
+        $thisRow.append(newBtn);
+      }
+      board.push(row);
+      vertBoard.push(column);
+    }
+    message("Game in play");
+    addClickHandlers();
+    return board;
+  };
+
+  playerMove = function(player, rowIndex, columnIndex) {
+    if (
+      board[rowIndex][columnIndex] === "X" ||
+      board[rowIndex][columnIndex] === "O"
+    ) {
+      console.log(`Played tile.`);
+    } else {
+      board[rowIndex][columnIndex] = player;
+      vertBoard[columnIndex][rowIndex] = player;
+      $("#" + rowIndex + "-" + columnIndex).addClass(player);
+      $("#" + rowIndex + columnIndex).css("background-color", "black");
+      getDiagBoard(board);
+      checkForWinner(player);
+      count++;
+      getPlayer(count);
+    }
+  };
+
+  // player = "X";
+  //From the selector we will get the player (either X or O)
+
+  //I must attach event handler to each of the boardBtns that calls playMove
+
+  // $(".boardBtn").on();
+
+  clearBoard = function() {
+    $(".gameBoard").empty();
+    createBoard(lastRowCall, lastColumnCall);
+  };
+
+  winStreak = function(player) {
+    const $winningVal = $("#winning-streak").val();
+    let winner = "";
+    for (var i = 0; i < $winningVal; i++) {
+      winner += player;
+    }
+    return winner;
+  };
+
+  checkForWinner = function(player) {
+    const winnerX = winStreak("X"),
+      winnerO = winStreak("O");
+    for (let i = 0; i < board[0].length; i++) {
       if (
-        board[x].join() === winnerArr.join() ||
-        vertArrs[y].join() === winnerArr.join() ||
-        diagArrs[0].join() === winnerArr.join() ||
-        diagArrs[1].join() === winnerArr.join()
+        board[i].join("").indexOf(winnerX) >= 0 ||
+        board[i].join("").indexOf(winnerO) >= 0
       ) {
-        this.scoreBoard(winnerArr.join());
-      }
-    },
-
-    scoreBoard: function(winner) {
-      if (winner === "X,X,X") {
-        xScore += 1;
-      } else {
-        oScore += 1;
-      }
-      $("#player-o-score").text(oScore);
-      $("#player-x-score").text(xScore);
-      setTimeout(function() {
-        alert(`Player ${winnerMessage} won`);
-      }, 200);
-      setTimeout(function() {
-        $(".gameBoard").empty();
-        $(".gameBoard").css("background", "none");
-        $(".gameBoard").css("background-image", "url(./images/therock.gif)");
-        $(".gameBoard").css("background-size", "cover");
-      }, 500);
-    },
-
-    playMove: function(player) {
-      if ($0select.hasClass("selected") || $Xselect.hasClass("selected")) {
-        let btnId = event.target.id;
-        btnId = btnId.split("-");
-        let x = btnId[0];
-        let y = btnId[1];
-        if (player === "oClicked") {
-          board[x][y] = "O";
-        } else {
-          board[x][y] = "X";
-        }
+        message(`${player} is the winner!!!`);
+        scoreBoard(player);
+        $(".boardBtn").off("click");
+        window.setTimeout(clearBoard, 3000);
       }
     }
-  }; // ticTac object
-
-  const newGame = function() {
-    $("#new-game").on("click", function() {
-      $("#0, #1, #2").remove(); //!!!HardCoded
-      $("board").empty();
-      ticTac.createBoard(3, 3, 0); //!!!HardCoded
-      addEventHandlers();
-    });
-  };
-
-  const addEventHandlers = function() {
-    $(".boardBtn").on("click", function() {
-      if ($Xselect.hasClass("selected") || $0select.hasClass("selected")) {
-        ticTac.switchPlayer();
-        $(this).addClass(player); //adds the visual represation to the board.
-        ticTac.playMove(player); //changes the element on board to X/O
-        let btnId = event.target.id;
-        ticTac.boardRules(btnId, player); //Checks for winner
-      } else {
-        alert("select player");
+    for (let j = 0; j < vertBoard.length; j++) {
+      if (
+        vertBoard[j].join("").indexOf(winnerX) >= 0 ||
+        vertBoard[j].join("").indexOf(winnerO) >= 0
+      ) {
+        message(`${player} is the winner!!!`);
+        scoreBoard(player);
+        $(".boardBtn").off("click");
+        window.setTimeout(clearBoard, 3000);
       }
-    });
+    }
+    for (let k = 0; k < diagBoard.length; k++) {
+      if (
+        diagBoard[k].join("").indexOf(winnerX) >= 0 ||
+        diagBoard[k].join("").indexOf(winnerO) >= 0
+      ) {
+        message(`${player} is the winner!!!`);
+        scoreBoard(player);
+        $(".boardBtn").off("click");
+        window.setTimeout(clearBoard, 3000);
+      }
+    }
   };
-  addEventHandlers();
-  newGame();
 }); //End document ready
+
+window.onload = function() {
+  document.getElementById("close-modal").onclick = function() {
+    const $inputRows = $("#controller-rows").val();
+    const $inputColumns = $("#controller-columns").val();
+    createBoard($inputRows, $inputColumns);
+    document.getElementById("modal").style.display = "none";
+    count = 1;
+    getPlayer(count);
+  };
+};
+
+//////player selector
